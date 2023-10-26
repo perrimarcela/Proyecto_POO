7fdec8999f943905d7c08c8c58f92ed957b8f060
Rafaferdez <108842229+Rafaferdez@users.noreply.github.com>
2023-10-26 19:56:58 -0300

    Huesped.java

--- ProyectoChekInn/src/ar/com/checkinn/proyecto/entities/Huesped.java
+++ ProyectoChekInn/src/ar/com/checkinn/proyecto/entities/Huesped.java
@@ -31,25 +31,41 @@ public class Huesped {
         return email;
     }
     public final void setEmail(String email) {
-        this.email = email;
+        if (email.length()<=40) {
+            this.email = email;
+        } else {
+            System.out.println("email demasiado largo");
+        }
     }
     public String getTelefono() {
         return telefono;
     }
     public final void setTelefono(String telefono) {
-        this.telefono = telefono;
+        if (telefono.length()<=10) {
+            this.telefono=telefono;
+        } else {
+            System.out.println("telefono demasiado largo");
+        }
     }
     public String getDireccion() {
         return direccion;
     }
     public final void setDireccion(String direccion) {
-        this.direccion = direccion;
+        if (direccion.length()<=50) {
+            this.direccion = direccion;
+        } else {
+            System.out.println("direccion demasiado largo");
+        }
     }
     public String getNumDocumento() {
         return numDocumento;
     }
     public final void setNumDocumento(String numDocumento) {
-        this.numDocumento = numDocumento;
+        if (numDocumento.length()<=8) {
+            this.numDocumento=numDocumento;
+        } else {
+            System.out.println("Documento demasiado largo");
+        }
     }
     public String getTipoDocumento() {
         return tipoDocumento;
@@ -61,13 +77,21 @@ public class Huesped {
         return apellido;
     }
     public final void setApellido(String apellido) {
-        this.apellido = apellido;
+        if (apellido.length()<=20) {
+           this.apellido = apellido;
+        } else {
+            System.out.println("Ingrese un apellido mas corto");
+        }
     }
     public String getNombre() {
         return nombre;
     }
     public final void setNombre(String nombre) {
-        this.nombre = nombre;
+        if (nombre.length()<=20) {
+           this.nombre = nombre; 
+        } else {
+            System.out.println("Ingrese un nombre mas corto");
+        }
     }
     public int getIdHuesped() {
         return idHuesped;
