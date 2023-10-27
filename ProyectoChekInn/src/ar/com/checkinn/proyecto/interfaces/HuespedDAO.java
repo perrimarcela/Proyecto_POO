0c18c282cb1cd9440d5814a5457a3006011bff67
Rafaferdez <108842229+Rafaferdez@users.noreply.github.com>
2023-10-27 18:47:22 -0300

    HuespedDAO.java

--- ProyectoChekInn/src/ar/com/checkinn/proyecto/interfaces/HuespedDAO.java
+++ ProyectoChekInn/src/ar/com/checkinn/proyecto/interfaces/HuespedDAO.java
@@ -1,13 +1,11 @@
 package ar.com.checkinn.proyecto.interfaces;
 
 import ar.com.checkinn.proyecto.entities.Huesped;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface HuespedDAO {
     List<Huesped> consultarTodos();
     void consultarUno(String numDocumento);
     void insertar(Huesped a);
-    void eliminar(String numDocumento);
-    ArrayList<Huesped> cargarLista();
+    void eliminar(String numDocumento)
 }
