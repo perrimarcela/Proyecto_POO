076805eda895b53e223f78c9c643bc296b3240fe
Rafaferdez <108842229+Rafaferdez@users.noreply.github.com>
2023-10-27 18:44:12 -0300

    Add files via upload

---
+++ ProyectoChekInn/src/ar/com/checkinn/proyecto/interfaces/HabitacionDAO.java
@@ -0,0 +1,14 @@
+package ar.com.checkinn.proyecto.interfaces;
+
+import ar.com.checkinn.proyecto.entities.Habitacion;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface HabitacionDAO {
+    List<Habitacion> consultarDisponibles();
+    void consultarUna(String numero);
+    void insertar(Habitacion a);
+    void eliminar(String numero);
+    ArrayList<Habitacion> cargarListaDisponibles();
+}
+
